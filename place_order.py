0e1b444601d7a0f92b578bac441cceccf215241b
bizAnurag1 <anurag.shimpi@bizmetric.com>
2024-08-22 12:24:54 +0530

    added requirements file

--- place_order.py
+++ place_order.py
@@ -4,8 +4,8 @@ from bill_generator import *
 restaurant = Restaurant(tbl_id=10)
 
 # Place orders
-order1 = restaurant.place_order(menu_id=4, quantity=2)
-order2 = restaurant.place_order(menu_id=8, quantity=3)
+order1 = restaurant.place_order(menu_id=5, quantity=2)
+order2 = restaurant.place_order(menu_id=6, quantity=3)
 
 if order1 or order2:
 # Generate the bill
